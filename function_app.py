e14c9135bba8122bfa38efa706eaaf3bede74d6d
Cristiano Alves <cristianosjunior2012@hotmail.com>
2025-12-03 16:42:13 -0300

    att dnv

--- function_app.py
+++ function_app.py
@@ -11,3 +11,34 @@ app = func.FunctionApp()
 def func_letrus(letrusTimer: func.TimerRequest) -> None:
     job.process_aux()
     job.process_silver()
+
+@app.timer_trigger(schedule="0 0 */2 * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
+def func_sql_to_supabase(myTimer: func.TimerRequest) -> None:
+    logging.info("⏰ Iniciando execução diária de múltiplas migrações")
+
+    base_path = Path(__file__).parent
+    config_path = base_path / "sql_to_supabase" /"config" / "mappings.json"
+
+    with open(config_path, "r", encoding="utf-8") as f:
+        mappings = json.load(f)
+
+    for item in mappings:
+        table_name = item["table_name"]
+        query_file = base_path / item["query_file"]
+
+        with open(query_file, "r", encoding="utf-8") as q:
+            query = q.read()
+
+        logging.info(f"🚀 Processando tabela '{table_name}'")
+
+        try:
+            # Limpa tabela
+            clear_supabase_table(table_name)
+
+            # Executa query e envia dados
+            insert_enriched_data(table_name, query)
+
+            logging.info(f"✅ Concluído: {table_name}")
+
+        except Exception as e:
+            logging.error(f"❌ Erro na tabela '{table_name}': {str(e)}")
